03ba521f930f8145c34139e74a850921fd5c5f04
Francis Valoy <francisalcantara001@gmail.com>
2024-04-05 23:32:44 -0400

    add fake group store

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,35 @@
+using Fair;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddSingleton<GroupStore>();
+
+app.MapGet("/groups", (GroupStore gStore) => Results.Ok(gStore.GetAll()));
 
 app.Run();
+
+public sealed class GroupStore
+{
+    private readonly Dictionary<GroupId, Group> _groups = new();
+
+    public List<Group> GetAll()
+        => _groups.Select(g => g.Value).ToList();
+
+    public Group GetById(string groupId)
+    {
+        _ = _groups.TryGetValue(new(groupId), out var group);
+        return group == null
+            ? throw new Exception("Could not found the group.")
+            : group;
+    }
+
+    public string Create()
+    {
+        var group = Group.Create(Enumerable.Empty<Member>().ToArray(), Enumerable.Empty<Contribution>().ToArray());
+        var result = _groups.TryAdd(group.Id, group);
+        return result == false
+            ? throw new Exception("Could not create the group.")
+            : group.Id.Value;
+    }
+}
